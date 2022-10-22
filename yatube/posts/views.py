8c6d75a1c6720c81c4d58b41096781c035579834
Oleg Grishin <grishin.codewar@gmail.com>
2022-10-22 21:06:35 +0400

    For third review

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -13,7 +13,7 @@ POST_PER_PAGE = 10
 
 def index(request):
     post_list = Post.objects.all()
-    page_obj = paginate_page(request, post_list, POST_PER_PAGE)
+    page_obj = paginate_page(request, post_list)
     context = {
         'page_obj': page_obj,
     }
@@ -23,7 +23,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     post_list = group.posts.all()
-    page_obj = paginate_page(request, post_list, POST_PER_PAGE)
+    page_obj = paginate_page(request, post_list)
     context = {
         'group': group,
         'page_obj': page_obj,
@@ -34,7 +34,7 @@ def group_posts(request, slug):
 def profile(request, username):
     author = get_object_or_404(User, username=username)
     post_list = author.posts.all()
-    page_obj = paginate_page(request, post_list, POST_PER_PAGE)
+    page_obj = paginate_page(request, post_list)
     context = {
         'page_obj': page_obj,
         'author': author,
@@ -43,14 +43,10 @@ def profile(request, username):
 
 
 def post_detail(request, post_id):
-    # Решил немного не так как Вы сказали
-    # Использовал get_object_or_404 для поста, а не для юзера
-    # Или Вы это и имели ввиду, а я не понял :)
     post = get_object_or_404(Post, pk=post_id)
-    author = post.author
     context = {
         'post': post,
-        'author': author,
+        'author': post.author,
     }
     return render(request, 'posts/post_detail.html', context)
 
@@ -62,29 +58,24 @@ def post_create(request):
         post = form.save(commit=False)
         post.author = request.user
         post.save()
-        return redirect("posts:profile", username=post.author.username)
+        return redirect('posts:profile', username=post.author.username)
 
     return render(request, 'posts/create_post.html', {'form': form})
 
 
 @login_required
 def post_edit(request, post_id):
-    # Добавил проверку на принадлежнолсть поста автору
-    # Мне кажется она нужна, так как через браузер
-    # Через инструменты разработчика, можно
-    # получить доступ к кнопке "Редаутировать"
-    # и присвоить себе чужой пост
     post = get_object_or_404(Post, pk=post_id)
     author = post.author
     if author != request.user:
-        return redirect("posts:post_detail", post_id)
+        return redirect('posts:post_detail', post_id)
 
     form = PostForm(request.POST or None, instance=post)
     if form.is_valid():
         post = form.save(commit=False)
         post.author = request.user
         post.save()
-        return redirect("posts:post_detail", post_id)
+        return redirect('posts:post_detail', post_id)
 
     context = {
         'form': form,
